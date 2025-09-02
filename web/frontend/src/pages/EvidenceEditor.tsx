c2874dc9c2c9aa53c94de1cc030796a3f4ae3783
Andrei Olar <andrei.olar@gmail.com>
2025-09-02 13:52:36 +0300

    move footer to react component

--- web/frontend/src/pages/EvidenceEditor.tsx
+++ web/frontend/src/pages/EvidenceEditor.tsx
@@ -46,33 +46,51 @@ export default function EvidenceEditor({evidence, fileName}: EvidenceProps) {
     const doiText = evidence.doi || "n/a"
     const dateText = (evidence.publishedAt || "n/a").toString()
     return (
-        <main className="main-container">
-            <div>
-                <div className="article-info">
-                    <div className="source-container">
-                        <SafeLink url={evidence.url} text={evidence.publicationVenue} label="Source:"
-                                  style={{fontSize: "12px", margin: "2px"}}/>
-                        <SafeLink url={evidence.doiLink} text={doiText} label="DOI:"/>
-                        <SafeLink url={evidence.sciHubLink} text={doiText} label="SciHub:"/>
+        <div>
+            <main className="main-container">
+                <div>
+                    <div className="article-info">
+                        <div className="source-container">
+                            <SafeLink url={evidence.url} text={evidence.publicationVenue} label="Source:"
+                                      style={{fontSize: "12px", margin: "2px"}}/>
+                            <SafeLink url={evidence.doiLink} text={doiText} label="DOI:"/>
+                            <SafeLink url={evidence.sciHubLink} text={doiText} label="SciHub:"/>
+                        </div>
+                        <small style={{fontSize: "9px", margin: "2px"}}>
+                            <b>Date Published:</b>&nbsp;
+                            {dateText}
+                        </small>
                     </div>
-                    <small style={{fontSize: "9px", margin: "2px"}}>
-                        <b>Date Published:</b>&nbsp;
-                        {dateText}
-                    </small>
+                    <b className="abstract-label">Abstract</b>
+                    <div className="scrollbox">
+                        {evidence.abstract}
+                    </div>
+                    <p className="keywords">
+                        <strong>Keywords:</strong>
+                        {evidence.keywords}
+                    </p>
                 </div>
-                <b className="abstract-label">Abstract</b>
-                <div className="scrollbox">
-                    {evidence.abstract}
+                <SelectionCriteriaForm evidenceId={evidence.clusterId}
+                                       fileName={fileName}
+                                       include={evidence.include}
+                                       exclusionCriteria={evidence.excludeReasons}
+                />
+            </main>
+            <footer className="footer-container">
+                <div className="button-bar">
+                    <form method="post" action={`/evidence/${fileName}/process-form`}>
+                        <input type="hidden" name="id" value={evidence.clusterId}/>
+                        <button type="submit" name="action" value="prev">Previous</button>
+                        <button type="submit" name="action" value="next">Next
+                        </button>
+                        <span className="goto-group">
+                            <input type="number" name="index" value={evidence.clusterId} style={{width: "4em"}}/>
+                            <button type="submit" name="action" value="goto">Go To</button>
+                        </span>
+                        <button type="submit" name="action" value="next-unfilled">First Unfilled</button>
+                    </form>
                 </div>
-                <p className="keywords">
-                    <strong>Keywords:</strong>
-                    {evidence.keywords}
-                </p>
-            </div>
-            <SelectionCriteriaForm evidenceId={evidence.clusterId}
-                                   fileName={fileName}
-                                   include={evidence.include}
-                                   exclusionCriteria={evidence.excludeReasons}
-            />
-        </main>)
+            </footer>
+        </div>
+    )
 }
